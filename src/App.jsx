2e57845b18bafbc61fe43f9514c9bb462e2ce307
mikus151 <mikus151@wp.pl>
2024-10-07 15:00:04 +0200

    add phots and some path

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,107 @@
-// import { useState } from 'react'
 import kierownicaImage from "./assets/photo/kierownica.webp";
+import kanapa_moto from "./assets/photo/fotel_zastawa.webp";
+import fotel_zastawa from "./assets/photo/kanapa_moto.webp";
 import maszynaImage from "./assets/photo/masyna.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
   faInstagram,
   faWhatsapp,
-  
-  
 } from "@fortawesome/free-brands-svg-icons";
 import { faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
-
-
 import "./App.css";
 
 function App() {
-const phoneNumber = "504311731";
+  const phoneNumber = "504311731";
 
   return (
     <>
       <header className="header">
-        <h4>Tapicer Jędrzejów</h4>
+        <h4>
+          Tapicer Jędrzejów <br />
+          <span className="header-address">ul. Strażacka 39</span>
+        </h4>
         <div>
           <a className="phone-info" href={`tel:${phoneNumber}`}>
             <FontAwesomeIcon icon={faPhoneAlt} size="lg" />
-            <span>tel. {phoneNumber}</span>
+            <span>tel. 504 - 311 - 731</span>
           </a>
         </div>
       </header>
 
       <div>
-        <h3>Budujemy dla was nową stronę. Za utrudnienia przepraszamy. </h3>
+        <h3>Budujemy dla was nową stronę. Za utrudnienia przepraszamy.</h3>
         <img src={maszynaImage} className="maszyna" alt="maszyna do szycia" />
       </div>
+
       <h1>
         <span className="companyName">Tapicer Jędrzejów</span> Zaprasza!
       </h1>
-      <h2>ul. Strażacka 39, 28-300 Jędrzejów</h2>
-      <h2>tel. 504 311 731</h2>
-      <img src={kierownicaImage} className="kierownica" alt="Kierownica" />
 
-      <div>
-        <h2>czym się zajmujemy:</h2>
-        <ul>
-          <li>obszywanie foteli samochodowych</li>
-          <li>regeneracja foteli po wystrzale AIRBAG</li>
-          <li>podsufitki odklejone i po wystrzale</li>
-          <li>obszywanie kierownic</li>
+      <h2>ul. Strażacka 39, 28-300 Jędrzejów</h2>
+
+      <div className="images">
+        <img src={kierownicaImage} className="kierownica" alt="kierownica" />
+        <img src={fotel_zastawa} className="kierownica" alt="kierownica" />
+        <img src={kanapa_moto} className="kierownica" alt="kierownica" />
+      </div>
+
+      <div >
+        <h2 >Czym się zajmujemy?</h2>
+        <h3>Głównie tapicerstwem samochodowym i motocyklowym:</h3>
+        <ul className="items-list">
+          <li>Obszywanie kanap motocyklowych</li>
+          <li>Obszywanie foteli samochodowych</li>
+          <li>Regeneracja foteli po wystrzale AIRBAG</li>
+          <li>Podsufitki odklejone i po wystrzale</li>
+          <li>Obszywanie kierownic</li>
           <li>
-            elementy mebli{" "}
+            Elementy mebli{" "}
             <b>(nie przyjmujemy całych kanap, foteli domowych i łóżek)</b>
           </li>
-          <li>inne - Po więcej zadzwoń i zapytaj</li>
+          <li>Inne - Po więcej zadzwoń i zapytaj</li>
         </ul>
       </div>
 
-      <h3>Budujemy dla was nową stronę. Za utrudnienia przepraszamy. </h3>
       <footer>
-        <h3>Znajdź nas na:</h3>
-        <div className="social-links">
-          <a
-            href="https://www.facebook.com/tapicerjedrzejow"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faFacebook} size="2x" />
-          </a>
-          <a
-            href="https://www.instagram.com/tapicerjedrzejow/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
-          </a>
-          <a
-            href="https://wa.me/504311731"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-          </a>
+        <div className="footer">
+          <div className="address">
+            <p className="footer-logo">Tapicer Jędrzejów</p>
+            <p>ul. Strażacka 39</p>
+            <p>28-300 Jędrzejów</p>
+            <p>tel. 504311731 </p>
+            <p>www.tapicerjedrzejow.pl</p>
+          </div>
+          <div>
+            <h2>Odszukaj nas:</h2>
+            <div className="social-links">
+              <a
+                href="https://www.facebook.com/tapicerjedrzejow"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+              </a>
+              <a
+                href="https://www.instagram.com/tapicerjedrzejow/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              </a>
+              <a
+                href="https://wa.me/504311731"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3>Budujemy dla was nową stronę. Za utrudnienia przepraszamy.</h3>
+          <p>Copyright by MARVI</p>
         </div>
       </footer>
     </>
